cc1e27ebe185d8edca8bb48294f12ea7dd454bb0
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-08-12 19:03:03 +0000

    レイアウト編集

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 
-  validates :comment, presence: true, length: { maximum: 35 }
+  validates :comment, presence: true, length: { maximum: 80 }
 end
