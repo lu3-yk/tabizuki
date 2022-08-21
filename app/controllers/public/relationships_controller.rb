75f06d3e0150b3149c743355e57e4eb539a292cb
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-08-21 05:51:32 +0000

    非同期追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,15 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
